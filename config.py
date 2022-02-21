75c6693a2a4dd96af93e7e3876fe3b81e8f9cb1c
kungfu-kenny <oleksandrshevchenko889@gmail.com>
2022-02-22 00:32:32 +0200

    added first values to work with

--- config.py
+++ config.py
@@ -1,8 +1,36 @@
 import os
+from dotenv import load_dotenv
 
 
+load_dotenv()
+
+token = os.getenv('TOKEN')
+users_resend = [os.getenv('USER_DEFAULT')]
 
 class Folders:
     folder_here = os.getcwd()
     folder_logs = 'logs'
+
+class Commands:
+    command_start = 'start'
+    command_settings = 'settings'
+    command_profile = 'profile'
+    command_users = 'users'
+    command_help = 'help'
+
+class Buttons:
+    button_settings = 'Settings'
+    button_profile = 'Profile'
+    button_help = 'Help'
+    button_users = 'Users'
+
+class InlineButtonSend:
+    button_sun = '1'
+    button_cloud = '2'
+    button_cock = '3'
+
+class InlineButtonAdd:
+    button_add = 'Add'
+    button_delete = 'Delete'
+    button_clear = 'Clear'
     
---
+++ views/views.py
@@ -0,0 +1,10 @@
+from aiogram import types
+from views.view_bot import bot, dp
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
